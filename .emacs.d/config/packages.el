96e2bd76a4e7b31eded5767494108b82bd38793d
memememomo <memememomo@gmail.com>
2012-08-14 14:15:16 +0900

    First import

---
+++ .emacs.d/config/packages.el
@@ -0,0 +1,239 @@
+(require 'package)
+
+
+;;;----------------------------------------------------------------------------------------------------
+;;; Auto Complete
+;; 自動補完
+(package-install 'github "m2ym/auto-complete" 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories
+             (format "%s/auto-complete/dict" package-base-dir))
+(ac-config-default)
+(add-hook 'auto-complete-mode-hook
+          (lambda ()
+            (define-key ac-completing-map (kbd "C-n") 'ac-next)
+            (define-key ac-completing-map (kbd "C-p") 'ac-previous)))
+
+;;;----------------------------------------------------------------------------------------------------
+;;; Anything
+;; iswitchbの代わり
+(let ((original-browse-url-browser-function browse-url-browser-function))
+  (setq anything-command-map-prefix-key "C-c C-<SPC>")
+  (package-install 'repo.or.cz '((files . ("anything-config"))
+                                 (additional-paths . ("extensions")))
+                   'anything-startup)
+  (define-key global-map (kbd "C-x b") 'anything-for-files)
+  (define-key anything-map (kbd "C-z") nil)
+  (define-key anything-map (kbd "C-l") 'anything-execute-persistent-action)
+  (define-key anything-map (kbd "C-o") nil)
+  (define-key anything-map (kbd "C-M-n") 'anything-next-source)
+  (define-key anything-map (kbd "C-M-p") 'anything-previous-source)
+  (setq browse-url-browser-function original-browse-url-browser-function))
+
+
+;;;----------------------------------------------------------------------------------------------------
+;; perl-completion
+
+(package-install 'emacswiki "perl-completion.el" 'perl-completion)
+(when (require 'perl-completion nil t)
+  (global-set-key (kbd "C-M-d") 'plcmp-cmd-show-doc)
+  (global-set-key (kbd "C-M-p") 'plcmp-cmd-show-doc-at-point))
+
+;;;----------------------------------------------------------------------------------------------------
+;; php-completion
+
+(package-install 'emacswiki "php-completion.el" 'php-completion)
+(add-hook 'php-mode-hook
+         (lambda ()
+             (require 'php-completion)
+             (php-completion-mode t)
+             (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
+             (when (require 'auto-complete nil t)
+             (make-variable-buffer-local 'ac-sources)
+             (add-to-list 'ac-sources 'ac-source-php-completion)
+             (auto-complete-mode t))))
+
+
+
+;;;----------------------------------------------------------------------------------------------------
+;;; perl-mode
+
+;; 参考 http://tech.lampetty.net/tech/index.php/archives/384
+
+(defalias 'perl-mode 'cperl-mode)
+(setq cperl-indent-level 4)
+(setq cperl-continued-statement-offset 4)
+(setq cperl-close-paren-offset -4)
+(setq cperl-label-offset -4)
+(setq cperl-comment-column 40)
+(setq cperl-highlight-variables-indiscriminately t)
+(setq cperl-indent-parens-as-block t)
+(setq cperl-tab-always-indent nil)
+;(setq cperl-electric-parens t) ; 対応する括弧自動挿入 うざい
+;(setq cperl-invalid-face nil)
+
+(setq cperl-indent-level 4
+      cperl-continued-statement-offset 4
+      cperl-close-paren-offset -4
+      cperl-label-offset -4
+      cperl-comment-column 40
+      cperl-highlight-variables-indiscriminately t
+      cperl-indent-parens-as-block t
+      cperl-tab-always-indent nil
+      cperl-font-lock t)
+
+; steal from perlhacks
+(global-set-key "\M-p" 'cperl-perldoc)
+
+(add-hook 'cperl-mode-hook '(lambda ()
+                              (setq indent-tabs-mode nil)
+                              ; BestPractices からぱくったがなんかうごいてない
+                              (setq fill-column 78)
+                              (setq auto-fill-mode t)
+                              ; face設定。これはどっかちがうとこにうつす
+                              (set-face-background 'cperl-hash-face (face-background 'default))
+                              (setq cperl-hash-face 'cperl-hash-face)
+                              ;(make-face 'cperl-array-face)
+                              ;(set-face-foreground 'cperl-array-face "color-69")
+                              (set-face-background 'cperl-array-face (face-background 'default))
+                              (setq cperl-array-face 'cperl-array-face)
+                              ))
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+	(t (self-insert-command (or arg 1)))))
+(global-set-key "%" 'match-paren)
+
+
+(add-hook  'cperl-mode-hook (lambda ()
+                              (require 'auto-complete)
+                              (require 'perl-completion)
+                              (add-to-list 'ac-sources 'ac-source-perl-completion)
+                              (perl-completion-mode t)))
+
+(add-to-list 'auto-mode-alist '("\\.t$" . perl-mode))
+
+
+;;;----------------------------------------------------------------------------------------------------
+;;; CSS Mode
+
+(autoload 'css-mode "css-mode")
+(setq auto-mode-alist
+      (cons '("\\.css\\'" . css-mode) auto-mode-alist))
+(setq cssm-indent-function #'cssm-c-style-indenter)
+
+
+
+;;;----------------------------------------------------------------------------------------------------
+;;; javascript Mode
+
+(add-to-list 'auto-mode-alist (cons "\\.js\\'" 'javascript-mode))
+(autoload 'javascript-mode "javascript" nil t)
+(setq js-indent-level 4)
+
+
+
+;;;----------------------------------------------------------------------------------------------------
+;;; run-test
+
+
+;;html
+;(autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
+(setq auto-mode-alist (cons '("\\.html$" . html-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.tmpl$" . html-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.html.ep" . html-mode) auto-mode-alist))
+;(setq auto-mode-alist (cons '("\\.tpl$" . html-helper-mode) auto-mode-alist))
+
+
+; 設定例
+(autoload 'php-mode "php-mode")
+(setq auto-mode-alist
+      (cons '("\\.php\\'" . php-mode) auto-mode-alist))
+(setq php-mode-force-pear t)
+(add-hook 'php-mode-user-hook
+  '(lambda ()
+     (setq indent-tabs-mode t)
+     (setq php-manual-path "/home/admin2/doc/php")
+     (setq php-manual-url "http://www.phppro.jp/phpmanual/")))
+
+
+(require 'sr-speedbar)
+(setq sr-speedbar-right-side nil)
+
+
+;; perldoc -m を開く
+
+;; モジュールソースバッファの場合はその場で、
+;; その他のバッファの場合は別ウィンドウに開く。
+(put 'perl-module-thing 'end-op
+     (lambda ()
+       (re-search-forward "\\=[a-zA-Z][a-zA-Z0-9_:]*" nil t)))
+(put 'perl-module-thing 'beginning-op
+     (lambda ()
+       (if (re-search-backward "[^a-zA-Z0-9_:]" nil t)
+           (forward-char)
+         (goto-char (point-min)))))
+
+(defun perldoc-m ()
+  (interactive)
+  (let ((module (thing-at-point 'perl-module-thing))
+        (pop-up-windows t)
+        (cperl-mode-hook nil))
+    (when (string= module "")
+      (setq module (read-string "Module Name: ")))
+    (let ((result (substring (shell-command-to-string (concat "perldoc -m " module)) 0 -1))
+          (buffer (get-buffer-create (concat "*Perl " module "*")))
+          (pop-or-set-flag (string-match "*Perl " (buffer-name))))
+      (if (string-match "No module found for" result)
+          (message "%s" result)
+        (progn
+          (with-current-buffer buffer
+            (toggle-read-only -1)
+            (erase-buffer)
+            (insert result)
+            (goto-char (point-min))
+            (cperl-mode)
+            (toggle-read-only 1)
+            )
+          (if pop-or-set-flag
+              (switch-to-buffer buffer)
+            (display-buffer buffer)))))))
+
+(global-set-key (kbd "C-M-m") 'perldoc-m)
+
+
+;; zencoding-mode
+(require 'zencoding-mode)
+(add-hook 'html-mode-hook 'zencoding-mode) ;; html-modeとかで自動出来にzencodingできるようにする
+(define-key zencoding-mode-keymap (kbd "C-c C-m") 'zencoding-expand-line)
+(define-key zencoding-preview-keymap (kbd "C-c C-m") 'zencoding-preview-accept)
+
+
+;; yasnippet
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/packages/yasnippet-0.5.10/snippets")
+
+
+;; howm
+(setq load-path (append
+		 '("~/.emacs.d/packages/howm")
+		 load-path))
+(setq howm-directory (concat user-emacs-directory "howm"))
+(setq howm-menu-lang 'ja)
+(setq howm-file-name-format "%Y/%m/%Y-%m-%d.howm")
+(when (require 'howm-mode nil t)
+  (define-key global-map (kbd "C-c ,,") 'howm-menu))
+
+(defun howm-save-buffer-and-kill ()
+  "howmメモを保存と同時に閉じます。"
+  (interactive)
+  (when (and (buffer-file-name)
+	     (string-match "\\.howm" (buffer-file-name)))
+    (save-buffer)
+    (kill-buffer nil)))
+(define-key howm-mode-map (kbd "C-c C-c") 'howm-save-buffer-and-kill)
+
+
---
+++ .emacs.d/init.el
@@ -0,0 +1,163 @@
+;;; ロードパス
+;; ロードパスの追加
+(setq load-path (append
+                 '("~/.emacs.d"
+                   "~/.emacs.d/packages")
+                 load-path))
+
+;;; 日本語環境
+;; Localeに合わせた環境の設定
+(set-locale-environment nil)
+
+;;日本語環境:UTF-8
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+
+;;; キーバインド
+;; 基本
+(define-key global-map (kbd "C-h") 'delete-backward-char) ; 削除
+(define-key global-map (kbd "M-?") 'help-for-help)        ; ヘルプ
+(define-key global-map (kbd "C-z") 'undo)                 ; undo
+(define-key global-map (kbd "C-c i") 'indent-region)      ; インデント
+(define-key global-map (kbd "C-c C-i") 'hippie-expand)    ; 補完
+(define-key global-map (kbd "C-c ;") 'comment-dwim)       ; コメントアウト
+(define-key global-map (kbd "M-C-g") 'grep)               ; grep
+(define-key global-map (kbd "C-[ M-C-g") 'goto-line)      ; 指定行へ移動
+(define-key global-map (kbd "M-I") 'indent-region)
+
+;; ウィンドウ移動
+;; 2011-02-17
+(define-key global-map (kbd "C-M-n") 'next-multiframe-window)
+(define-key global-map (kbd "C-M-p") 'previous-multiframe-window)
+
+;;; grep
+;; 再帰的にgrep
+;; 2011-02-18
+(require 'grep)
+(setq grep-command-before-query "grep -nH -r -e ")
+(defun grep-default-command ()
+  (if current-prefix-arg
+      (let ((grep-command-before-target
+             (concat grep-command-before-query
+                     (shell-quote-argument (grep-tag-default)))))
+        (cons (if buffer-file-name
+                  (concat grep-command-before-target
+                          " *."
+                          (file-name-extension buffer-file-name))
+                (concat grep-command-before-target " ."))
+              (+ (length grep-command-before-target) 1)))
+    (car grep-command)))
+;(setq grep-command (cons (concat grep-command-before-query " .")
+
+;;; バー
+;; メニューバーを消す
+(menu-bar-mode -1)
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+
+;;; カーソル
+;; カーソルの点滅を止める
+(blink-cursor-mode 0)
+
+
+;;; 括弧
+;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+;; ウィンドウ内に収まらないときだけ括弧内も光らせる。
+(setq show-paren-style 'mixed)
+
+
+;; 行末の空白を表示
+(setq-default show-trailing-whitespace t)
+
+
+
+;; 現在行を目立たせる(文字が崩れるのでオフにしてる)
+;(global-hl-line-mode)
+
+;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+
+;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+;; カーソルの場所を保存する
+(require 'saveplace)
+(setq-default save-place t)
+
+
+;;; 行
+;; 行の先頭でC-kを一回押すだけで行全体を消去する
+(setq kill-whole-line t)
+;; 最終行に必ず一行挿入する
+(setq require-final-newline t)
+;; バッファの最後でnewlineで新規行を追加するのを禁止する
+(setq next-line-add-newlines nil)
+
+
+;;; バックアップ
+;; バックアップファイルを作らない
+
+;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+
+;;; 補完
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+;; 部分一致の補完機能を使う
+;; p-bでprint-bufferとか
+(partial-completion-mode t)
+;; 補完可能なものを随時表示
+;; 少しうるさい
+(icomplete-mode 1)
+
+
+;;; 履歴
+;; 履歴数を大きくする
+(setq history-length 10000)
+;; ミニバッファの履歴を保存する
+(savehist-mode 1)
+;; 最近開いたファイルを保存する数を増やす
+(setq recentf-max-saved-items 10000)
+
+;; ediffを1ウィンドウで実行
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; diffのオプション
+(setq diff-switches '("-u" "-p" "-N"))
+
+;; diredを便利にする
+(require 'dired-x)
+;; diredから"r"でファイル名をインライン編集する
+(require 'wdired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+;;; バッファ名
+;; ファイル名が重複していたらディレクトリ名を追加する。
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;;; リージョンの大文字小文字変換を有効にする。
+;; C-x C-u -> upcase
+;; C-x C-l -> downcase
+;; 2011-03-09
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+
+;;; 追加の設定
+(load "config/builtins")
+;; 非標準Elispの設定
+(load "config/packages")
+
+(put 'set-goal-column 'disabled nil)
+
+
+(server-start)
